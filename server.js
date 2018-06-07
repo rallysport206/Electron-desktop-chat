74bf352db39a4560987019adb863be57ac7bd4ec
rallysport206 <phan.tony125@gmail.com>
2018-06-07 11:40:56 -0700

    updated with files. Tested with server to make username with success!

--- server.js
+++ server.js
@@ -1,40 +1,38 @@
-// ./server.js
-
 const express = require('express')
-const bodyParsher = require('body-parser')
-const cors = require ('cors')
-const Chatkit = require ('pusher-chatkit-server')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const Chatkit = require('pusher-chatkit-server')
 
 const chatkit = new Chatkit.default({
-    instanceLocator: 'v1:us1:660404d4-3b36-4539-9c7a-38cf1adf8930',
-    key: '571c2d7d-3454-4a0f-a89e-1a6dd48f34a9:37NZG04QFzkk8EB0lZ2Yo/akSsY6tklhTAD+jjgGxis='
+  instanceLocator: 'v1:us1:660404d4-3b36-4539-9c7a-38cf1adf8930',
+  key:
+    '571c2d7d-3454-4a0f-a89e-1a6dd48f34a9:37NZG04QFzkk8EB0lZ2Yo/akSsY6tklhTAD+jjgGxis='
 })
-
 const app = express()
 
-app.use(bodyParsher.urlencoded({ extended:false }))
-app.use(bodyParsher.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(cors())
 
 app.post('/users', (req, res) => {
-    const { username } = req.body
-    const user = { name: username, id: username }
-    chatkit
-        .createUser(user)
-        .then(() => {
-            console.log('Created user ', user.name)
-            res.status(201).jason(user)
-        })
-        .catch(error => {
-            if (error.error === 'services/chatkit/user_already_exists') {
-                console.log('User already exists ', user.name)
-                res.status(201).jason(user)
-            } else {
-                console.error(error)
-                res.status(error.status).jason(error)
-            }
-        })
+  const { username } = req.body
+  const user = { name: username, id: username }
+  chatkit
+    .createUser(user)
+    .then(() => {
+      console.log('Created user ', user.name)
+      res.status(201).json(user)
+    })
+    .catch(error => {
+      if (error.error === 'services/chatkit/user_already_exists') {
+        console.log('User already exists ', user.name)
+        res.status(201).json(user)
+      } else {
+        console.error(error)
+        res.status(error.status).json(error)
+      }
+    })
 })
 
 app.listen(3001)
-console.log('running on port 3001')
+console.log('Running on port 3001')
